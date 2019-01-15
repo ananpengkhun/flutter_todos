818fea81fab0a8a94f391e11945bb2687f904794
menza01 <se56160422@gmail.com>
2019-01-15 17:47:27 +0700

    new release

--- lib/setting/SettingPage.dart
+++ lib/setting/SettingPage.dart
@@ -1,16 +1,40 @@
 import 'package:flutter/material.dart';
 
-class SettingPage extends StatelessWidget {
+class SettingPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _SettingPageState();
+  }
+}
+
+class _SettingPageState extends State<SettingPage> {
+
+
   @override
   Widget build(BuildContext context) {
-    return Stepper(steps: [
-      Step(content: Text("content"), title: Text("title")),
-      Step(content: Text("content"), title: Text("title")),
-      Step(content: Text("content"), title: Text("title")),
-      Step(content: Text("content"), title: Text("title")),
-      Step(content: Text("content"), title: Text("title")),
-      Step(content: Text("content"), title: Text("title")),
-      Step(content: Text("content"), title: Text("title")),
-    ]);
+    return Container(
+      color: Colors.pink,
+    );
   }
+
+//  Future<Null> _login() async {
+//    FacebookLoginResult _facebookResult =
+//        await _facebookLogin.logInWithReadPermissions(['email']);
+//    switch (_facebookResult.status) {
+//      case FacebookLoginStatus.loggedIn:
+//        FacebookAccessToken token = _facebookResult.accessToken;
+//        _showMessage(token.token);
+//        break;
+//      case FacebookLoginStatus.error:
+//        break;
+//      case FacebookLoginStatus.cancelledByUser:
+//        break;
+//    }
+//  }
+//
+//  _showMessage(String token){
+//    setState(() {
+//      this.token = token;
+//    });
+//  }
 }
